6159ad1c4da236c849539f209f0714d0fc0f407a
Giseth Diaz <gisethasdisu612@gmail.com>
2025-11-27 01:12:31 -0500

    bosque

--- JUEGO/Scenes/Bosque/bosque.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var player=$AudioStreamPlayer
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	player.play()
-	pass # Replace with function body.
